448e5907c0a364ea94ff711472c8737c0c67eec1
Bao Nguyen <bnguyen88@knights.ucf.edu>
2023-06-20 10:02:21 +0700

    adding file

---
+++ ReadMe.md
@@ -0,0 +1,49 @@
+# aaaaa     ![alt text](https://img.shields.io/badge/license-Mozilla_Public_License_2.0-blue)
+
+## Description
+
+- What was your motivation?
+    sdasdasd
+
+- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
+    sdasdasd
+
+- What problem does it solve?
+    asdasdasdsa
+
+- What did you learn?
+    asdasdasdas
+
+## Table of Contents:
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#how-to-contribute)
+- [Test](#tests)
+- [Question](#questions)
+
+## Installation
+
+What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+    sadasdasda
+
+## Usage
+    asdasdas
+
+## License
+
+The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+
+    🏆 Mozilla Public License 2.0 : (https://choosealicense.com/licenses/mpl-2.0/)
+    
+    Permissions of this weak copyleft license are conditioned on making available source code of licensed files and modifications of those files under the same license (or in certain cases, one of the GNU licenses). Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. However, a larger work using the licensed work may be distributed under different terms and without source code for files added in the larger work.    
+
+## Contributing
+    sdasdas
+
+## Tests
+    asdasdas
+
+## Questions
+    GitHub User name : asdasdsa
+    Email address : asdasdsadsa
---
+++ index.js
@@ -0,0 +1,195 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+// // TODO: Create an array of questions for user input
+// const questions = [];
+
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
+const licenseInfo = [
+    {
+        licenseList: 'GNU AGPLv3',
+        information: 'Permissions of this strongest copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. When a modified version is used to provide a service over a network, the complete source code of the modified version must be made available.',
+        link: 'https://choosealicense.com/licenses/agpl-3.0/',
+        badge: 'https://img.shields.io/badge/license-GNU_AGPLv3-blue'
+    },
+    {
+        licenseList: 'GNU GPLv3',
+        information: 'Permissions of this strong copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights.',
+        link: 'https://choosealicense.com/licenses/gpl-3.0/',
+        badge: 'https://img.shields.io/badge/license-GNU_GPLv3-blue'
+    },
+    {
+        licenseList: 'GNU LGPLv3',
+        information: 'Permissions of this copyleft license are conditioned on making available complete source code of licensed works and modifications under the same license or the GNU GPLv3. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. However, a larger work using the licensed work through interfaces provided by the licensed work may be distributed under different terms and without source code for the larger work.',
+        link: 'https://choosealicense.com/licenses/lgpl-3.0/',
+        badge: 'https://img.shields.io/badge/license-GNU_LGPLv3-blue'
+    },
+    {
+        licenseList: 'Mozilla Public License 2.0',
+        information: 'Permissions of this weak copyleft license are conditioned on making available source code of licensed files and modifications of those files under the same license (or in certain cases, one of the GNU licenses). Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. However, a larger work using the licensed work may be distributed under different terms and without source code for files added in the larger work.',
+        link: 'https://choosealicense.com/licenses/mpl-2.0/',
+        badge: 'https://img.shields.io/badge/license-Mozilla_Public_License_2.0-blue'
+    },
+    {
+        licenseList: 'Apache License 2.0',
+        information: 'A permissive license whose main conditions require preservation of copyright and license notices. Contributors provide an express grant of patent rights. Licensed works, modifications, and larger works may be distributed under different terms and without source code.',
+        link: 'https://choosealicense.com/licenses/apache-2.0/',
+        badge: 'https://img.shields.io/badge/license-Apache_License_2.0-blue'
+    },
+    {
+        licenseList: 'MIT License',
+        information: 'A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code.',
+        link: 'https://choosealicense.com/licenses/mit/',
+        badge: 'https://img.shields.io/badge/license-MIT-blue'
+    },
+    {
+        licenseList: 'Boost Software License 1.0',
+        information: 'A simple permissive license only requiring preservation of copyright and license notices for source (and not binary) distribution. Licensed works, modifications, and larger works may be distributed under different terms and without source code.',
+        link: 'https://choosealicense.com/licenses/bsl-1.0/',
+        badge: 'https://img.shields.io/badge/license-Boost_Software_License_1.0-blue'
+    },
+    {
+        licenseList: 'The Unlicense',
+        information: 'A license with no conditions whatsoever which dedicates works to the public domain. Unlicensed works, modifications, and larger works may be distributed under different terms and without source code.',
+        link: 'https://choosealicense.com/licenses/unlicense/',
+        badge: 'https://img.shields.io/badge/license-The_Unlicense-blue'
+    }
+]
+
+const generateHTML = ({ title, discription1,discription2,discription3,discription4,installation,Usage,license,contributing,test,github,email},licenseInformation,link,badgeEL) =>
+`# ${title}     ![alt text](${badgeEL})
+
+## Description
+
+- What was your motivation?
+    ${discription1}
+
+- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
+    ${discription2}
+
+- What problem does it solve?
+    ${discription3}
+
+- What did you learn?
+    ${discription4}
+
+## Table of Contents:
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#how-to-contribute)
+- [Test](#tests)
+- [Question](#questions)
+
+## Installation
+
+What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+    ${installation}
+
+## Usage
+    ${Usage}
+
+## License
+
+The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+
+    🏆 ${license} : (${link})
+    
+    ${licenseInformation}    
+
+## Contributing
+    ${contributing}
+
+## Tests
+    ${test}
+
+## Questions
+    GitHub User name : ${github}
+    Email address : ${email}
+`;
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is your projetc title?',
+    },
+    {
+      type: 'input',
+      name: 'discription1',
+      message: 'What was your motivation?',
+    },
+    {
+        type: 'input',
+        name: 'discription2',
+        message: 'Why did you build this project?',
+      },
+      {
+        type: 'input',
+        name: 'discription3',
+        message: 'What problem does it solve?',
+      },
+      {
+        type: 'input',
+        name: 'discription4',
+        message: 'What did you learn?',
+      },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'How do you install your app in step by step?',
+    },
+    {
+      type: 'input',
+      name: 'Usage',
+      message: 'What is your Usage?',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Choose your license which you use.',
+      choices: ['GNU AGPLv3','GNU GPLv3','GNU LGPLv3','Mozilla Public License 2.0','Apache License 2.0','MIT License','Boost Software License 1.0','The Unlicense']
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'What is your contributing?',
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'What is your test?',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your Github username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    }
+  ])
+  .then((answers) => {
+    
+    for( let i = 0; i < licenseInfo.length; i++){
+        if(answers.license === licenseInfo[i].licenseList){
+            var licenseInformation = licenseInfo[i].information;
+            var link = licenseInfo[i].link;
+            var badgeEL = licenseInfo[i].badge;
+            const htmlPageContent = generateHTML(answers,licenseInformation,link,badgeEL);
+            fs.writeFile('ReadMe.md', htmlPageContent, (err) =>
+            err ? console.log(err) : console.log('Successfully created ReadMe.md!')
+            );
+        }
+    }
+  });
